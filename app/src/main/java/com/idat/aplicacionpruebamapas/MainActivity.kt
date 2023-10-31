974654d053d580037349dc5d13fd42f2d5a3da39
osdan97 <daniejurade@gmail.com>
2023-10-30 21:54:33 -0500

    implements routes

---
+++ app/src/main/java/com/idat/aplicacionpruebamapas/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.idat.aplicacionpruebamapas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map:GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+    }
+}
--- app/src/main/java/pe/idat/frontend/api/models/SignUpResponse.kt
+++
@@ -1,8 +0,0 @@
-package pe.idat.frontend.api.models
-
-data class SignUpResponse(
-    val email: String,
-    val uuid: String,
-    val name: String,
-    val lastname: String
-)
